3e5b2b284e08095abf477b9f2ccde359be07a662
harshR <42332761+tetrex@users.noreply.github.com>
2023-11-17 21:25:12 +0530

    methods , with multiple functions

--- methods.go
+++ methods.go
@@ -9,11 +9,16 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt((*v).X*v.X + (*v).Y*v.Y)
 }
 
+func (v *Vertex) Abs2() float64 {
+	return ((*v).X*2 + (*v).Y*2)
+}
 func main() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
+	v2 := Vertex{2, 4}
+	fmt.Println(v2.Abs2())
 }
