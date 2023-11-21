e988db50a05f8df58acb42d9643a1519e4e2eb27
harshR <42332761+tetrex@users.noreply.github.com>
2023-11-21 15:22:14 +0530

    faet: stringer exec

---
+++ stringer.exc.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+func (v *IPAddr) String() string {
+	return fmt.Sprintf("%v", v)
+}
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
