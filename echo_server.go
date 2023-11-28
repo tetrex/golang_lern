69e11186a5238083e63671e298a41a18ccb5cd34
harshR <42332761+tetrex@users.noreply.github.com>
2023-11-28 16:06:28 +0530

    feat : echo , swagger test

--- echo_server.go
+++ echo_server.go
@@ -6,13 +6,42 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	// Route
+	// ShowAccount godoc
+	// @Summary      Show an account
+	// @Description  get string by ID
+	// @Tags         accounts
+	// @Accept       json
+	// @Produce      json
+	// @Success      200  {object}  String
+	// @Failure      400  {object}  httputil.HTTPError
+	// @Failure      404  {object}  httputil.HTTPError
+	// @Failure      500  {object}  httputil.HTTPError
+	// @Router       /hello [get]
 	e.GET("/hello", hello)
 
 	e.Logger.Fatal(e.Start(":1323"))
